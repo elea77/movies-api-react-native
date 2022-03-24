caa47003eea8bbbeb4d7bc52d3a7d96194bd8156
eleacarton <eleacarton@me.com>
2022-03-24 15:30:14 +0100

    debug movieList

--- src/config/routes/moviesStack.js
+++ src/config/routes/moviesStack.js
@@ -4,14 +4,14 @@ import Home from '../../screens/home'
 import Movie from '../../screens/movie'
 import MoviesList from '../../screens/moviesList'
 import Details from '../../screens/details'
-import Wishlist from '../../screens/wishlist'
 
 const Stack = createNativeStackNavigator()
 
 const MoviesStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Wishlist' component={Wishlist} />
+      <Stack.Screen name='Cinéma' component={Home} />
+      <Stack.Screen name='Liste des films' component={MoviesList} />
       <Stack.Screen name='Film' component={Movie} />
       <Stack.Screen name='Details' component={Details} />
     </Stack.Navigator>
